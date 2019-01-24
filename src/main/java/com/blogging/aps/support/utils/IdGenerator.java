8d334dacbc5a8591b61d5e83bc0d4b81e7fba982
techoneduan <techoneduan@blackfish.cn>
2019-01-24 16:20:13 +0800

    snow flake

---
+++ src/main/java/com/blogging/aps/support/utils/IdGenerator.java
@@ -0,0 +1,29 @@
+package com.blogging.aps.support.utils;
+
+/**
+ * @author techoneduan
+ * @date 2019/1/22
+ */
+public class IdGenerator {
+
+    private static final String POST_PRE = "PO";
+
+    private static final String PASSAGE_PRE = "PA";
+
+
+    /**
+     * post id生成
+     *
+     * @return
+     */
+    public static String generatePostId () {
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public static String generatePassageId () {
+        return null;
+    }
+}
